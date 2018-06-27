73f640b64ddbdd1cd05b9d4b82cfb73f34578fbd
Boaz Nechemia <boazn1@gmail.com>
2018-06-27 13:13:20 +0300

    Michsot payload

---
+++ Edu.Mzt.Partners.Interfaces/MosadKita/Michsot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edu.Mzt.Partners.Interfaces.MosadKita
+{
+    public class Michsa
+    {
+        public int SHNAT_LIMUDIM { get; set; }
+        public int SEMEL_MOSAD { get; set; }
+        public int SHICHVA { get; set; }
+        public int SUG_KITA { get; set; }
+        public int MISPAR_KITOT_MEUSHAROT { get; set; }
+        public DateTime TAARICH_HATCHALA { get; set; }
+        public DateTime? TAARICH_SIYUM { get; set; }
+        public DateTime TAARICH_IDKUN { get; set; }
+        public string CODE_MISHTAMESH_MEADKEN { get; set; }
+    }
+
+}
