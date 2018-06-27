2e000b7cc824883967a7212de8af81dda7530e41
kl1069 <kl1069@VS2015DEV02>
2018-06-27 16:00:29 +0300

    Adding entities for: TalmidInfo, TalmidKesher, GormeyKesher

---
+++ Edu.Mzt.Partners.Interfaces/Talmid/GoremKesherKtovet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edu.Mzt.Partners.Interfaces.Talmid
+{
+    public class GoremKesherKtovet
+    {
+        public int CODE_YEUD_KTV_G_KESHER { get; set; }
+        public int SEMEL_YISHUV_G_KESHER { get; set; }
+        public int SEMEL_RECHOV_G_KESHER { get; set; }
+        public string TEUR_KTOVET_G_KESHER { get; set; }
+        public int MISPAR_BAYIT_G_KESHER { get; set; }
+        public string KNISA_G_KESHER { get; set; }
+        public int MISPAR_DIRA_G_KESHER { get; set; }
+        public int MIKUD_G_KESHER { get; set; }
+        public int TEVAT_DOAR_G_KESHER { get; set; }
+        public string DOAR_NA_G_KESHER { get; set; }
+        public string SHCHUNA_G_KESHER { get; set; }
+        public DateTime TAARICH_IDKUN_KTV_G_KESHER { get; set; }
+        public string CODE_MISHTAMESH_MEADKEN_KTV_G_KESHER { get; set; }
+        public DateTime TAARICH_IDKUN_RESHUMA_KTV_G_KESHER { get; set; }
+
+    }
+
+}
---
+++ Edu.Mzt.Partners.Interfaces/Talmid/TalmidKesher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edu.Mzt.Partners.Interfaces.Talmid
+{
+    public class TalmidKesher
+    {
+        public int SUG_ZEHUT_TALMID { get; set; }
+        public int MISPAR_ZEHUT_TALMID { get; set; }
+        public int SEMEL_MOSAD_TALMID { get; set; }
+        public int CODE_YEUD_KTV_NOSAF_TAL { get; set; }
+        public int SEMEL_YISHUV_NOSAF_TAL { get; set; }
+        public int SEMEL_RECHOV_NOSAF_TAL { get; set; }
+        public string TEUR_KTOVET_NOSAF_TAL { get; set; }
+        public int MISPAR_BAYIT_NOSAF_TAL { get; set; }
+        public string KNISA_NOSAF_TAL { get; set; }
+        public int MISPAR_DIRA_NOSAF_TAL { get; set; }
+        public int MIKUD_NOSAF_TAL { get; set; }
+        public int TEVAT_DOAR_NOSAF_TAL { get; set; }
+        public string DOAR_NA_NOSAF_TAL { get; set; }
+        public string SHCHUNA_NOSAF_TAL { get; set; }
+        public DateTime TAARICH_IDKUN_KTV_NOSAF_TAL { get; set; }
+        public string CODE_MISHTAMESH_MEADKEN_KTV_NOSAF_TAL { get; set; }
+        public DateTime TAARICH_IDKUN_RESHUMA_KTV_NOSAF_TAL { get; set; }
+        public string EMAIL_RASHI_TAL { get; set; }
+        public DateTime TAARICH_IDKUN_EMAIL_RASHI_TAL { get; set; }
+        public string CODE_MISHTAMESH_MEADKEN_EMAIL_RASHI_TAL { get; set; }
+        public string EMAIL_NOSAF_TAL { get; set; }
+        public DateTime TAARICH_IDKUN_EMAIL_NOSAF_TAL { get; set; }
+        public string CODE_MISHTAMESH_MEADKEN_EMAIL_NOSAF_TAL { get; set; }
+       
+    }
+}
