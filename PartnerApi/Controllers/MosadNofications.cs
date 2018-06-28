e3133bfb52e065f5f3e89cd2ebdd64774ca2ce57
Avior Nissim <aviorni@education.gov.il>
2018-06-28 13:51:13 +0300

    Mosad Models And API changes

--- PartnerApi/Controllers/MosadNofications.cs
+++ PartnerApi/Controllers/MosadNofications.cs
@@ -3,6 +3,13 @@ using Edu.Mzt.Partners.Interfaces.Talmid;
 using PartnerApi.AppCode;
 using System.Web.Http;
 
+//MosadNotifications/KitaInfo
+//MosadNotifications/MoadInfo
+//MosadNotifications/MegamaInfo
+//MosadNotifications/KitaMichsa
+
+
+
 namespace PartnerApi.Controllers
 {
     [AuthorizeCallerByHeader()]
@@ -10,22 +17,38 @@ namespace PartnerApi.Controllers
     public class MosadNoficationsController : ApiController
     {
         
-        [HttpPost]       
-        [Route("MosadInfo")]
-        public IHttpActionResult MosadInfo([FromBody] MosadKita data)
+        //[HttpPost]       
+        //[Route("MosadInfo")]
+        //public IHttpActionResult MosadInfo([FromBody] MosadKita data)
+        //{
+        //    // save the data to the db here..   
+        //    return Ok(data);
+        //}
+
+        [HttpPost]
+        [Route("KitaInfo")]
+        public IHttpActionResult KitaInfo([FromBody] MosadKita data)
         {
             // save the data to the db here..   
             return Ok(data);
         }
 
+        [HttpPost]
+        [Route("MegamaInfo")]
+        public IHttpActionResult MegamaInfo([FromBody] MegamaInfo data)
+        {
+            // save the data to the db here..   
+            return Ok(data);
+        }
 
-        //[HttpPost]
-        //[Route("MosadMegama")]
-        //public IHttpActionResult MosadMegama([FromBody] TalmidShibutz data)
-        //{
-        //    var tz = data.MISPAR_ZEHUT;
-        //    return Ok(data);
-        //}
+        [HttpPost]
+        [Route("KitaMichsa")]
+        public IHttpActionResult KitaMichsa([FromBody] Michsa data)
+        {
+            // save the data to the db here..   
+            return Ok(data);
+        }
         
+
     }
 }
